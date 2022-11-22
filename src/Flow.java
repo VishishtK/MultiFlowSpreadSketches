5e8b0d85fe30c4512e0b9d26bd4e1c5f4899caab
vishishtK <vishisht.khilariwal@gmail.com>
2022-11-21 19:05:49 -0500

    Flow created

--- src/Flow.java
+++ src/Flow.java
@@ -1,10 +1,15 @@
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Flow {
     String flowId;
     int numberOfPackets;
     int estimatedNumberOfPackets;
+    int[] packets;
 
     public Flow(String flowID, int numberOfPackets){
         this.flowId = flowID;
         this.numberOfPackets = numberOfPackets;
+        this.packets = IntStream.generate(() -> new Random().nextInt()).limit(numberOfPackets).map(flowId -> Math.abs(flowId)).toArray();
     }
 }
